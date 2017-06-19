e5b3c5bb1cd378c883620a5e78dd9053cef4d69e
Victor Rentea <victorrentea@gmail.com>
2017-06-19 10:07:19 +0300

    little renaming

--- src/main/java/victor/clean/lambdas/DeathOfTemplateMethod.java
+++ src/main/java/victor/clean/lambdas/DeathOfTemplateMethod.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-public class DeathOfTemplateMethod {
+class EmailService {
 
 	public void sendOrderReceivedEmail(String emailAddress) {
 		EmailContext context = new EmailContext(/*smtpConfig,etc*/);
