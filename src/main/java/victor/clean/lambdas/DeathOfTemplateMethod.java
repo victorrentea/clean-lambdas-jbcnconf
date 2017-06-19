de46356de3d9a61c41ea0b283770646de52248d9
Victor Rentea <victorrentea@gmail.com>
2017-06-19 13:59:09 +0300

    death of template

--- src/main/java/victor/clean/lambdas/DeathOfTemplateMethod.java
+++ src/main/java/victor/clean/lambdas/DeathOfTemplateMethod.java
@@ -1,14 +1,18 @@
 package victor.clean.lambdas;
 
 import java.util.Random;
+import java.util.function.Consumer;
 
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+//@Sevice 
 class EmailService {
+//	@Autowired
+//	private MyRepo myRepo;
 
-	public void sendOrderReceivedEmail(String emailAddress) {
+	public void sendEmail(String emailAddress, Consumer<Email> fillEmail ) {
 		EmailContext context = new EmailContext(/*smtpConfig,etc*/);
 		int MAX_RETRIES = 3;
 		for (int i = 0; i < MAX_RETRIES; i++ ) {
@@ -16,13 +20,35 @@ class EmailService {
 			email.setSender("noreply@corp.com");
 			email.setReplyTo("/dev/null");
 			email.setTo(emailAddress);
-			email.setSubject("Order Received");
-			email.setBody("Thank you for your order");
+			fillEmail.accept(email);
 			boolean success = context.send(email);
 			if (success) break;
 		}
 	}
 }
+class ClientCode {
+	protected static void fillReceivedEmail(Email email) {
+		email.setSubject("Order Received");
+		email.setBody("Thank you for your order");
+	}
+	protected static void fillShippedEmail(Email email) {
+		email.setSubject("Order Shipped");
+		email.setBody("Your order has been shipped (with a bird)");
+	}
+	
+	public static void main(String[] args) {
+		new EmailService().sendEmail("a@b.com", ClientCode::fillReceivedEmail);
+		new EmailService().sendEmail("a@b.com", ClientCode::fillShippedEmail);
+	}
+}
+
+
+
+
+
+
+
+
 
 class EmailContext {
 	public boolean send(Email email) {
@@ -30,6 +56,7 @@ class EmailContext {
 		return new Random(System.nanoTime()).nextBoolean();
 	}
 }
+
 
 @ToString
 class Email {
--- src/main/java/victor/clean/lambdas/higher/Compose.java
+++ src/main/java/victor/clean/lambdas/higher/Compose.java
@@ -1,20 +1,10 @@
 package victor.clean.lambdas.higher;
 
-import java.util.function.Function;
-
 public class Compose {
 	
 	public static void main(String[] args) {
 		String s = "a <    B";
-		Function<String, String> normalize = String::toUpperCase;
-		if (somt) {
-			normalize = normalize.andThen(Compose::escapeHtml);
-		} else {
-			normalize = normalize.andThen(Compose::escapeMarkDown);
-		}
-		normalize = normalize.andThen(Compose::collapseSpaces);
-		s = normalize.apply(s);
-//		s = collapseSpaces(escapeHtml(s.toUpperCase())); // that's a lot simpler ! just do f(g(h(x)))
+		s = collapseSpaces(escapeHtml(s.toUpperCase())); // that's a lot simpler ! just do f(g(h(x)))
 		System.out.println(s);
 	}
 
