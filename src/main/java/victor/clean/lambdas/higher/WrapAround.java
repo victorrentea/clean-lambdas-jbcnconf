976dd8c39ead3c2dcec8dd0897c4f6ab85015c3c
Victor Rentea <victorrentea@gmail.com>
2017-06-19 12:50:26 +0300

    wrap around some existing block - 'manual AOP'

--- src/main/java/victor/clean/lambdas/higher/WrapAround.java
+++ src/main/java/victor/clean/lambdas/higher/WrapAround.java
@@ -2,12 +2,28 @@ package victor.clean.lambdas.higher;
 
 public class WrapAround {
 	public static void main(String[] args) {
-		insertLanguages();
-		insertCountries();
+		executeInTx(WrapAround::insertLanguages);
+		executeInTx(WrapAround::insertCountries);
 	}
 	
+	public static void measureTime(Runnable functionToMeasure) {
+		long t0 = System.nanoTime();
+		functionToMeasure.run();
+		long t1 = System.nanoTime();
+		System.out.println("Took " + (t1-t0));
+	}
 	
-	// System.nanoTime();
+	public static void executeInTx(Runnable function) {
+		try {
+			System.out.println("Start Tx");
+			function.run();
+			System.out.println("Commit Tx");
+		}catch (RuntimeException e) {
+			System.out.println("Rollback Tx");
+		} finally {
+			System.out.println("Relesase Tx resource");
+		}
+	}
 
 	// existing
 	public static void insertLanguages() {
