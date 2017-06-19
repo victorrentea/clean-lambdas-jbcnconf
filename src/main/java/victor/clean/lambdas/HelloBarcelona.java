204b03739b7033613bdec36849f52451496170d3
Victor Rentea <victorrentea@gmail.com>
2017-06-19 12:13:38 +0300

    various ways -- pair program

--- src/main/java/victor/clean/lambdas/HelloBarcelona.java
+++ src/main/java/victor/clean/lambdas/HelloBarcelona.java
@@ -1,5 +1,33 @@
 package victor.clean.lambdas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HelloBarcelona {
 
+	public static void main(String[] args) {
+		List<Order> orders = new ArrayList<>();
+		
+		boolean found = orders.stream()
+			.anyMatch(order -> order.isNotDelivered());
+//		
+//		boolean found = orders.stream()
+//			.filter(order -> order.isNotDelivered())
+//			.count() >= 1;
+////		
+//		boolean found = orders.stream()
+//			.filter(order -> order.isNotDelivered())
+//			.findAny()
+//			.isPresent();
+////		
+//		boolean found = !orders.stream()
+//			.filter(order -> order.isNotDelivered())
+//			.collect(toList())
+//			.isEmpty();
+////		
+//		boolean found = orders.stream()
+//			.filter(order -> order.isNotDelivered())
+//			.collect(toList())
+//			.size() >= 1;
+	}
 }
