2d6ae10ced102eb407d71790568db7c087c23167
Victor Rentea <victorrentea@gmail.com>
2017-06-19 12:18:06 +0300

    Optional vs NPE

--- src/main/java/victor/clean/lambdas/OptionalVsNPE.java
+++ src/main/java/victor/clean/lambdas/OptionalVsNPE.java
@@ -1,5 +1,7 @@
 package victor.clean.lambdas;
 
+import java.util.Optional;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,17 +9,26 @@ public class OptionalVsNPE {
 
 	public static void main(String[] args) {
 		Customer customer = new Customer();
-		double discount = customer.getProfile().getGoldCard().getDiscount();
+		double discount= customer.getProfileOpt()
+				.flatMap(Profile::getGoldCardOpt)
+				.map(GoldCard::getDiscount)
+				.orElse(0d);
 		System.out.println("discount:  "+discount);
 	}
 }
 
 class Customer {
-	@Getter @Setter private Profile profile;
+	@Setter private Profile profile;
+	public Optional<Profile> getProfileOpt() {
+		return Optional.ofNullable(profile);
+	}
 }
 
 class Profile {
-	@Getter @Setter private GoldCard goldCard;
+	@Setter private GoldCard goldCard;
+	public Optional<GoldCard> getGoldCardOpt() {
+		return Optional.ofNullable(goldCard);
+	}
 }
 
 class GoldCard {
